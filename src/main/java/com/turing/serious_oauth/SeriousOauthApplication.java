7970aec46d2303dce4083e2134903246ff1352f7
Mudi Lukman <mudi.lukman.developer@gmail.com>
2025-10-27 23:46:17 +0100

    Init

---
+++ src/main/java/com/turing/serious_oauth/SeriousOauthApplication.java
@@ -0,0 +1,13 @@
+package com.turing.serious_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriousOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriousOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turing/serious_oauth/SeriousOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turing.serious_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriousOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
